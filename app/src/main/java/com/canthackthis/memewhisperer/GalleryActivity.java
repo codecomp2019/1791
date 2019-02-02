d9427f4762bf069491bfa3af9fdccab704c4148b
622 <max.hops.710@my.csun.edu>
2019-02-02 15:23:41 -0800

    OCR almost working

--- app/src/main/java/com/canthackthis/memewhisperer/GalleryActivity.java
+++ app/src/main/java/com/canthackthis/memewhisperer/GalleryActivity.java
@@ -6,16 +6,21 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public class GalleryActivity extends AppCompatActivity {
     private static int RESULT_LOAD_IMG = 1;
+    private Bitmap selectedImage = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +37,30 @@ public class GalleryActivity extends AppCompatActivity {
                 startActivityForResult(intent,RESULT_LOAD_IMG);
             }
         });
+        final ImageView imageView = findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+                Frame frame = new Frame.Builder().setBitmap(selectedImage).build();
+                Toast.makeText(getApplicationContext(), convertDetectToString(textRecognizer.detect(frame)),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 
+    protected String convertDetectToString(SparseArray<TextBlock> text){
+
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i=0; i<text.size(); i++){
+           // Toast.makeText(getApplicationContext(),text.valueAt(i).getValue(),Toast.LENGTH_LONG).show();;
+            String str = text.valueAt(i).getValue();
+            stringBuilder.append(str + " ");
+
+
+        }
+        return stringBuilder.toString();
     }
 
     @Override
@@ -44,7 +72,7 @@ public class GalleryActivity extends AppCompatActivity {
             try {
                 final Uri imageUri = data.getData();
                 final InputStream imageStream = getContentResolver().openInputStream(imageUri);
-                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);//Image bitmap
+                selectedImage = BitmapFactory.decodeStream(imageStream);//Image bitmap
                 final ImageView imageView = findViewById(R.id.imageView);
                 imageView.setImageBitmap(selectedImage);
 
