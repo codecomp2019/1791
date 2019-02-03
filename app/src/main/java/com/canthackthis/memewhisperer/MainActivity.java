0e6d0956567fcc176e1c8734b871106adafd22c6
Kevin <kevin.pavelich@yahoo.com>
2019-02-02 19:30:07 -0800

    #1

--- app/src/main/java/com/canthackthis/memewhisperer/MainActivity.java
+++ app/src/main/java/com/canthackthis/memewhisperer/MainActivity.java
@@ -2,12 +2,67 @@ package com.canthackthis.memewhisperer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private Button getBtn;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        result = findViewById(R.id.result);
+        getBtn = findViewById(R.id.getBtn);
+        getBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getContext();
+            }
+        });
+
+    }
+
+    private void getContext(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuilder builder = new StringBuilder();
+
+                try {
+                    Document doc = Jsoup.connect("https://knowyourmeme.com/memes/bad-luck-brian").get();
+                    String title = doc.title();
+                    Element content = doc.getElementById("entry_body");
+                    Element about = content.getElementsByTag("p").first();
+
+                    String context = ("About the meme: " + about.text());
+
+                    builder.append(title).append("\n");
+                    builder.append("\n").append(context);
+
+                    // builder.append("\n").append("About : ").append(about.attr("p")).append(about.text());
+
+                } catch (IOException e) {
+                    builder.append("error : ").append(e.getMessage()).append("\n");
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        result.setText(builder.toString());
+                    }
+                });
+            }
+        }).start();
     }
 }
+
